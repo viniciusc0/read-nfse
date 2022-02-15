2aa0116bb5d5392874388337bc7fc7d9e28d3191
Vinicius Oliveira <vinicius.oliveira@mosaico.com.br>
2022-02-15 08:05:35 -0300

    Save

--- src/api/nfe/nfeXml.ts
+++ src/api/nfe/nfeXml.ts
@@ -2,7 +2,7 @@ import ApiResponseHandler from "../apiResponseHandler";
 import NfeServices from "../../services/NfeServices";
 var convert = require("xml2json");
 import formidable from "formidable";
-import fs from 'fs'
+import fs from "fs";
 export default async (req, res, next) => {
   try {
     const form = formidable({});
@@ -15,33 +15,29 @@ export default async (req, res, next) => {
 
       // console.log(files['files.xml'].filepath)
       // return
-      const content = fs.readFile(files['files.xml'].filepath, (error, data) => {
-        if (error) {
-          next(error);
-          return;
+      const content = fs.readFile(
+        files["files.xml"].filepath,
+        (error, data) => {
+          if (error) {
+            next(error);
+            return;
+          }
+          const content = data;
+          // console.log(content.toString())
+          readContent(content.toString());
+          return content.toString();
         }
-        const content = data;
-        // console.log(content.toString())
-        readContent(content.toString())
-        return content.toString()
-      });
+      );
 
-
-
-     async  function readContent(content){
-
-          let obj = convert.toJson(content)
-          obj = JSON.parse(obj)
-          // console.log(obj)
+      async function readContent(content) {
+        let obj = convert.toJson(content);
+        obj = JSON.parse(obj);
+        // console.log(obj)
 
         const payload = await new NfeServices(obj).readXml();
         await ApiResponseHandler.success(req, res, payload);
       }
-
-      
     });
-
-
   } catch (error) {
     await ApiResponseHandler.error(req, res, error);
   }
